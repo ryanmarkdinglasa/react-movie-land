f1e52d86a25ebfd62745462260e1a7538777ef61
Mark Dinglasa <ryanmark.dinglasa@gmail.com>
2024-02-27 15:51:31 +0800

    InitialCommit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+ // key =  a04e7b9f
+ // API = http://www.omdbapi.com/?i=tt3896198&apikey=a04e7b9f
+
+
+const App = () => {
+    return (
+        <>
+            <h1>hellow</h1>
+        </>
+    )
+}
+
+export default App
